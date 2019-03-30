a86313979454d012f9839dc6baabf3c5818f77c8
Georgios Pardalis <georgpardal@gmail.com>
2019-03-31 00:33:20 +0200

    Separated test files

--- ride/ride_test.go
+++ ride/ride_test.go
@@ -4,22 +4,6 @@ import (
 	"testing"
 )
 
-func TestNewPosition(t *testing.T) {
-	position := NewPosition(1.00, 1.00, 1553984501)
-
-	if position.ts != 1553984501 {
-		t.Errorf("Expected ts: %d, got: %d", 1553984501, position.ts)
-	}
-
-	if position.longitude != 1.00 {
-		t.Errorf("Expected longituted: %f, got: %f", 1.00, position.longitude)
-	}
-
-	if position.latitude != 1.00 {
-		t.Errorf("Expected latitude: %f, got: %f", 1.00, position.latitude)
-	}
-}
-
 func TestNewRide(t *testing.T) {
 	var positions []Position
 
