69e478c720b628b169eca888a4160781590a02de
Georgios Pardalis <georgpardal@gmail.com>
2019-03-31 10:29:27 +0300

    Added e2e test case, along with test input

--- main_test.go
+++ main_test.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
+	"log"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
 func TestPipelineEndToEnd(t *testing.T) {
+	absInputFile, _ := filepath.Abs("./test_utils/dummyInput.csv")
+	absOutputFile, _ := filepath.Abs("dummyOutput.csv")
 
+	cliArgs := []string{"dummyApp", absInputFile, absOutputFile}
+	os.Args = cliArgs
+
+	main()
+
+	csvFile, err := os.Open(absOutputFile)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+	line, err := reader.Read()
+
+	if line[0] != "1" || line[1] != "3.47" {
+		t.Errorf("Excpected output: %d,%f Got: %s,%s", 1, 3.47, line[0], line[1])
+	}
+
+	csvFile.Close()
+	os.Remove(absOutputFile)
 }
